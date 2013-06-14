8cc5313417a94d869e3025e353abcd47aa88b1f8
Mahesh Sankhala <mahesh.saini@srijan.in>
2013-06-14 17:10:29 +0530

    comment added

--- cowsay.sh
+++ cowsay.sh
@@ -5,15 +5,6 @@ files=( $(find $cowfiledir -maxdepth 1 -type f) )
 randomfileindex=$((RANDOM%${#files[@]}+0))
 # echo $randomfileindex
 randomcowfile=${files[$randomfileindex]}
+# remove -o from next line if you don't want offensive fortune :P
 fortune -os | cowsay -f $randomcowfile
 
-# echo ${#cowfiles[@]}
-# echo $cowfiles;
-# for cowfile in $cowfiles
-# do
-#   echo "$cowfile"
-# done
-# for f in *.pdf
-# do
-#   echo "Removing password for pdf file - $f"
-# done
